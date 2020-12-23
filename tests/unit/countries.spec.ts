83d324f19622119bbb2699fe072a4ddb12a34426
DESKTOP-NRHBDJ6\EliteBook <quanminhle199x@gmail.com>
2020-12-24 01:24:06 +0700

    update code

--- tests/unit/countries.spec.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from "@vue/test-utils";
-import Countries from "@/components/AppCovid/Countries/Countries.vue";
-describe("Countries.vue", () => {
-  test("convertDate", () => {
-    const countries = mount(Countries);
-    expect(countries.vm.searchCountries).toBeTruthy();
-  });
-});
